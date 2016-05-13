836321ceb90acad22383a7b0b5dbf7ef1681ad88
Brad Hilton <brad.hilton.nw@gmail.com>
2016-05-13 08:41:24 -0600

    Add tests

--- Tests/OrderedObjectSetTests.swift
+++ Tests/OrderedObjectSetTests.swift
@@ -7,8 +7,39 @@
 //
 
 import XCTest
-@testable import OrderedObjectSet
+import OrderedObjectSet
 
 class OrderedObjectSetTests: XCTestCase {
+    
+    func testOrderedObjectSet() {
+        let brad: NSString = "Brad"
+        let lorraine: NSString = "Lorraine"
+        let evan: NSString = "Evan"
+        let scott: NSString = "Scott"
+        let sarah: NSString = "Sarah"
+        let ivonne: NSString = "Ivonne"
+        let natalie: NSString = "Natalie"
+        var orderedObjectSet: OrderedObjectSet<NSString> = [brad, lorraine, brad, sarah]
+        XCTAssert(orderedObjectSet == [brad, lorraine, sarah])
+        orderedObjectSet.append(evan)
+        XCTAssert(orderedObjectSet == [brad, lorraine, sarah, evan])
+        orderedObjectSet.insert(evan, atIndex: 0)
+        XCTAssert(orderedObjectSet == [evan, brad, lorraine, sarah])
+        orderedObjectSet[2] = brad
+        XCTAssert(orderedObjectSet == [evan, brad, sarah])
+        orderedObjectSet.unionInPlace([brad, scott, ivonne])
+        XCTAssert(orderedObjectSet == [evan, sarah, brad, scott, ivonne])
+        orderedObjectSet.subtractInPlace([brad, evan])
+        XCTAssert(orderedObjectSet == [sarah, scott, ivonne])
+        orderedObjectSet.intersectInPlace([sarah, brad, ivonne])
+        XCTAssert(orderedObjectSet == [sarah, ivonne])
+        orderedObjectSet.appendContentsOf([natalie, lorraine])
+        orderedObjectSet.exclusiveOrInPlace([ivonne, brad, natalie])
+        XCTAssert(orderedObjectSet == [sarah, lorraine, brad])
+        orderedObjectSet = orderedObjectSet + [lorraine, natalie]
+        XCTAssert(orderedObjectSet == [sarah, brad, lorraine, natalie])
+        orderedObjectSet += [sarah, sarah, scott, scott]
+        XCTAssert(orderedObjectSet == [brad, lorraine, natalie, sarah, scott])
+    }
     
 }
