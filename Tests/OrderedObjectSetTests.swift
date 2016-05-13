98c07a29cb389f089a7d83ad2b23c178f876ebda
Brad Hilton <brad.hilton.nw@gmail.com>
2016-05-13 08:36:40 -0600

    Add files

---
+++ Tests/OrderedObjectSetTests.swift
@@ -0,0 +1,14 @@
+//
+//  OrderedObjectSetTests.swift
+//  OrderedObjectSetTests
+//
+//  Created by Bradley Hilton on 5/13/16.
+//  Copyright © 2016 Brad Hilton. All rights reserved.
+//
+
+import XCTest
+@testable import OrderedObjectSet
+
+class OrderedObjectSetTests: XCTestCase {
+    
+}
